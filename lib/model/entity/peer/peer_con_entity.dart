d0b1b05143c110272b981729541a29016cff4363
chouette11 <keifukuda1115@gmail.com>
2024-01-03 16:02:27 +0900

    Fix peer entity

---
+++ lib/model/entity/peer/peer_con_entity.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:peerdart/peerdart.dart';
+
+part 'peer_con_entity.freezed.dart';
+
+@freezed
+class PeerConEntity with _$PeerConEntity {
+  const PeerConEntity._();
+
+  const factory PeerConEntity({
+    String? uid,
+    required String peerId,
+    required DataConnection con,
+  }) = _PeerConEntity;
+
+}
