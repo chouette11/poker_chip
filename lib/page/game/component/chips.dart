157e5bf5850b0155a29f4c85932de72a8e7bb941
chouette11 <keifukuda1115@gmail.com>
2024-01-14 19:10:26 +0900

    Add chip value

--- lib/page/game/component/chips.dart
+++ lib/page/game/component/chips.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:poker_chip/provider/presentation_providers.dart';
 import 'package:poker_chip/util/constant/color_constant.dart';
 
 class Chips extends StatelessWidget {
@@ -14,41 +16,53 @@ class Chips extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         mainAxisSize: MainAxisSize.min,
         children: [
-          Chip(color: Color(0xFF74AA9C), size: 64),
-          Chip(color: Color(0xFFA58CEA), size: 64),
-          Chip(color: Color(0xFF23ACD8), size: 64),
-          Chip(color: Color(0xFFFFC03F), size: 64),
-          Chip(color: Color(0xFFFF7D34), size: 64),
+          Chip(color: Color(0xFF74AA9C), size: 64, value: 500),
+          Chip(color: Color(0xFFA58CEA), size: 64, value: 300),
+          Chip(color: Color(0xFF23ACD8), size: 64, value: 100),
+          Chip(color: Color(0xFFFFC03F), size: 64, value: 50),
+          Chip(color: Color(0xFFFF7D34), size: 64, value: 10),
         ],
       ),
     );
   }
 }
 
-class Chip extends StatelessWidget {
-  const Chip({super.key, required this.color, required this.size});
+class Chip extends ConsumerWidget {
+  const Chip({
+    super.key,
+    required this.color,
+    required this.size,
+    required this.value,
+  });
+
   final Color color;
   final double size;
+  final int value;
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          width: size,
-          height: size,
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            color: ColorConstant.black90
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GestureDetector(
+      onTap: () {
+        ref.read(raiseBetProvider.notifier).update((state) => state + value);
+      },
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: size,
+            height: size,
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle, color: ColorConstant.black90),
           ),
-        ),
-        SvgPicture.asset(
-          'assets/images/chip.svg',
-          width: size,
-          height: size,
-          color: color,
-        ),
-      ],
+          SvgPicture.asset(
+            'assets/images/chip.svg',
+            width: size,
+            height: size,
+            color: color,
+          ),
+          Text(value.toString())
+        ],
+      ),
     );
   }
 }
