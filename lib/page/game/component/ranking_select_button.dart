937631c61ced93f32d6f5366d48386b8c97b5aeb
chouette11 <keifukuda1115@gmail.com>
2024-01-18 20:16:08 +0900

    Add ranking select button

---
+++ lib/page/game/component/ranking_select_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RankingSelectButton extends StatelessWidget {
+  const RankingSelectButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
