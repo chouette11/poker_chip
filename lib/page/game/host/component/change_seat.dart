a6fc6ce76c82212aa6bc481654c12860ce9d2776
chouette11 <keifukuda1115@gmail.com>
2024-03-10 23:45:01 +0900

    Add change seat widget

---
+++ lib/page/game/host/component/change_seat.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:poker_chip/provider/presentation/change_seat.dart';
+import 'package:poker_chip/util/constant/color_constant.dart';
+import 'package:poker_chip/util/constant/text_style_constant.dart';
+
+class ChangeSeatWidget extends ConsumerWidget {
+  const ChangeSeatWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final changeSeat = ref.watch(changeSeatProvider);
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 2),
+        borderRadius: BorderRadius.circular(8)
+      ),
+      child: Text(
+        changeSeat.isEmpty ? 'ユーザーをタップして\n席替えができます' : '交換先のユーザーを\nタップしてください',
+        textAlign: TextAlign.center,
+        style: TextStyleConstant.bold14.copyWith(color: ColorConstant.black10),
+      ),
+    );
+  }
+}
