ad296fd1a8ecaf5e65d899660a23f7e14304c0af
chouette11 <keifukuda1115@gmail.com>
2024-03-14 18:59:40 +0900

    Fix usage

--- lib/page/root/component/usage_button.dart
+++ lib/page/root/component/usage_button.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:poker_chip/util/constant/color_constant.dart';
 import 'package:poker_chip/util/constant/context_extension.dart';
+import 'package:poker_chip/util/constant/text_style_constant.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-class UsageButton extends StatefulWidget {
+class UsageButton extends StatelessWidget {
   const UsageButton({super.key});
 
   @override
-  State<UsageButton> createState() => _UsageButtonState();
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return UsageDialog();
+          },
+        );
+      },
+      child: const Text('使い方'),
+    );
+  }
 }
 
-class _UsageButtonState extends State<UsageButton> {
+class UsageDialog extends StatefulWidget {
+  const UsageDialog({super.key});
+
+  @override
+  State<UsageDialog> createState() => _UsageDialogState();
+}
+
+class _UsageDialogState extends State<UsageDialog> {
   int index = 0;
   final pages = [
     const _Page1(),
@@ -20,35 +41,43 @@ class _UsageButtonState extends State<UsageButton> {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              content: Padding(
-                  padding: const EdgeInsets.all(8), child: pages[index]),
-              actions: [
-                Row(
-                  children: [
-                    GestureDetector(child: Icon(Icons.arrow_left)),
-                    Text('${index + 1} / ${pages.length}'),
-                    GestureDetector(
-                        onTap: () {
-                          if (index == pages.length - 1) {
-                            return;
-                          }
-                          index++;
-                        },
-                        child: Icon(Icons.arrow_right)),
-                  ],
-                )
-              ],
-            );
-          },
-        );
-      },
-      child: const Text('使い方'),
+    return AlertDialog(
+      content: Padding(padding: const EdgeInsets.all(8), child: pages[index]),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                if (index == 0) {
+                  return;
+                }
+                index--;
+                setState(() {});
+              },
+              child: Icon(
+                Icons.arrow_left,
+                size: 48,
+              ),
+            ),
+            Text('${index + 1} / ${pages.length}'),
+            GestureDetector(
+              onTap: () {
+                print(index);
+                if (index == pages.length - 1) {
+                  return;
+                }
+                index++;
+                setState(() {});
+              },
+              child: Icon(
+                Icons.arrow_right,
+                size: 48,
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
@@ -63,9 +92,15 @@ class _Page1 extends StatelessWidget {
       width: 400,
       child: Column(
         children: [
-          const Text('使い方'),
-          const Text('1. 人数分のアプリが必要です'),
-          const Text('一緒にプレイする人がまだインストールしていない場合は以下のQRコードからインストールしてください。'),
+          Text(
+            '1. 人数分のアプリが必要です',
+            style:
+                TextStyleConstant.bold18.copyWith(color: ColorConstant.black10),
+          ),
+          const Text(
+            '一緒にプレイする人がまだインストールしていない場合は以下のQRコードからインストールしてください',
+            style: TextStyleConstant.text,
+          ),
           SizedBox(child: QrImageView(data: 'https://onelink.to/pabhbm'))
         ],
       ),
@@ -83,12 +118,15 @@ class _Page2 extends StatelessWidget {
       width: 400,
       child: Column(
         children: [
-          const Text('使い方'),
-          const Text('2. 部屋を作成'),
           Text(
-              '誰か一人が\'${context.l10n.makeRoom}\'から部屋を作成してください。部屋作成者はユーザーを二人タップすることで席替えができます。'),
-          SizedBox(child: QrImageView(data: 'https://onelink.to/pabhbm'))
-        ],
+            '2. 部屋作成',
+            style:
+            TextStyleConstant.bold18.copyWith(color: ColorConstant.black10),
+          ),
+          const Text(
+            '誰か一人が\'部屋作成\'から部屋を作成してください。部屋作成者はユーザーを二人タップすることで席替えが出来ます',
+            style: TextStyleConstant.text,
+          ),        ],
       ),
     );
   }
@@ -104,10 +142,15 @@ class _Page3 extends StatelessWidget {
       width: 400,
       child: Column(
         children: [
-          const Text('使い方'),
-          const Text('3. 部屋に参加'),
-          Text('他の人は\'${context.l10n.join}\'から部屋に参加し、RoomIDを入力してください。'),
-          SizedBox(child: QrImageView(data: 'https://onelink.to/pabhbm'))
+          Text(
+            '3. 部屋に参加',
+            style:
+            TextStyleConstant.bold18.copyWith(color: ColorConstant.black10),
+          ),
+          const Text(
+            '他の人は\'参加する\'から部屋に参加し、RoomIDを入力してください。',
+            style: TextStyleConstant.text,
+          ),
         ],
       ),
     );
@@ -124,10 +167,8 @@ class _Page4 extends StatelessWidget {
       width: 400,
       child: Column(
         children: [
-          const Text('使い方'),
           const Text('4. その他'),
           const Text('初期stackを変更したい場合は右上の設定から変更してください。'),
-          SizedBox(child: QrImageView(data: 'https://onelink.to/pabhbm'))
         ],
       ),
     );
